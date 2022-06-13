3044a90e3e6c1910867acf1a131a4133f79b8aca
Vinod D Pai <44208193+vinod-dpai@users.noreply.github.com>
2022-06-13 17:52:17 +0000

    Updated Code - Counter Application

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { incrementByAmount, decrementByAmount } from './redux/counter';
+
 function App() {
+  const { count } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const [number, setNumber] = useState(1);
+
+  const handleNumberChange = (e) => {
+    const { value } = e.target;
+    setNumber(parseInt(value));
+  };
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Count is {count}</h1>
+      <input type="number" value={number} onChange={handleNumberChange} />
+      <button type="button" onClick={() => dispatch(incrementByAmount(number))}>
+        +
+      </button>
+      <button type="button" onClick={() => dispatch(decrementByAmount(number))}>
+        -
+      </button>
     </div>
   );
 }
